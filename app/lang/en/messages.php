731b45c3afd76c892e50a2f667b2f5f8c093c69b
Thomas <tsp.thomas@gmail.com>
2014-09-03 21:58:33 -0300

    Small corrections on English translation

--- app/lang/en/messages.php
+++ app/lang/en/messages.php
@@ -7,22 +7,22 @@ return array(
 	'home' => 'Home',
 	'problems' => 'Problems',
 	'submit_problem' => 'Submit a Problem',
-	'sing_up' => 'Sing Up',
+	'sign_up' => 'Sign Up',
 	'log_in' => 'Log-in',
 	'solve_biggest_problems' => 'Together we can solve the <strong>BIGGEST</strong></br> problems of our times',
 	'tell_me_more' => 'Tell Me More',
 	'we_believe_that_together' => 'We believe that <strong>together</strong> we can find a solution for the biggest problems of our time',
-	'the_idea_is' => 'The idea is to isolate the biggest problems of our time, and, create a clever way to discuss about it
-                    each of us have a opinion now is time to put it together and find a real solution for those problems',
+	'the_idea_is' => 'The idea is to isolate the biggest problems and discuss about it in a smart way.
+                    Each of us have a different opinion on how to solve many things. Now, it is time to put together and find a real solution for those problems.',
    	'step1' => 'Step 1 - Understand the problem',
    	'step2' => 'Step 2 - Create you on solution',
    	'step3' => 'Step 3 - Contribute',
-   	'step1_description' => 'The main point in this fase is to understand the problem and try to get a lot of information about it.',
-   	'step2_description' => 'Now is time to think, how would be a good way to solve this problem, in this fase would be nice to make a step back and try to formulate some thing new.',
-   	'step3_description' => 'Discuss with the community to propose and improve the solution for the problem.',
-   	'look_current_problems' => 'Let\'s take a look on the problems that we are fixing right now. Don\'t forget to let your opinion about this problems.',
+   	'step1_description' => 'The main point in this step is to understand the problem and get a lot of information about it.',
+   	'step2_description' => 'Now, it is time to think how would be a good way to solve this problem. It would be nice to make a step back and formulate some thing new.',
+   	'step3_description' => 'Discuss with the community to propose and improve the problem\'s solution.',
+   	'look_current_problems' => 'Let\'s take a look on the problems that we are working on. Do not forget to tell your opinion about this problems.',
    	'ready_to_share' => 'Ready to share your <strong>ideas</strong>?',
-   	'you_can_sing_up' => 'You can sign up to Yoaky to share your ideas',
+   	'you_can_sign_up' => 'You can sign up to Yoaky to share your ideas',
    	'see_more' => 'See More'              
 
 );
