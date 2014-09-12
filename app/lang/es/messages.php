b8f77b2fa4bf9043c86b284b7b1884f2a6aea88d
Luis Calderon <lafcalderon@gmail.com>
2014-09-11 21:54:02 -0300

    Spanish strings translates

---
+++ app/lang/es/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	// HOME PAGE
+	'changing_the_world' 		=> 'Cambiando el mundo con ideas',
+	'home' 						=> 'Home',
+	'problems' 					=> 'Problemas',
+	'submit_problem' 			=> 'Presentar un Problema',
+	'sing_up' 					=> 'Inscribir',
+	'log_in' 					=> 'Iniciar Sesión',
+	'solve_biggest_problems' 	=> 'Juntos podemos resolver los <strong>MAYORES</strong></br> problemas de nuestro tiempo',
+	'tell_me_more' 				=> 'Quiero Saber Más',
+	'we_believe_that_together' 	=> 'Creemos que <strong>juntos</strong> podemos encontrar una solución para los problemas más grandes de nuestro tiempo',
+	'the_idea_is' 				=> 'La idea es aislar los mayores problemas de nuestro tiempo y crear una forma inteligente de hablar sobre el. Cada uno de nosotros tiene una opinión ahora es el momento de poner juntos y encontrar una solución real para los problemas',
+	'step1' 					=> 'Paso 1 - Comprender el problema',
+   	'step2' 					=> 'Paso 2 - Crear su propia solución',
+   	'step3' 					=> 'Paso 3 - Contribuir',
+   	'step1_description' 		=> 'El punto principal en esta etapa es entender el problema y tratar de obtener una gran cantidad de información.',
+   	'step2_description' 		=> 'Ahora es tiempo de pensar como seria una buena manera de resolver este problema. En esta primera etapa sería bueno dar un paso atrás y tratar de formular alguna cosa nueva.',
+   	'step3_description' 		=> 'Discuta con la comunidad para proponer y mejorar la solución para el problema.',
+   	'look_current_problems' 	=> 'Miremos los problemas que estamos arreglando en estos momentos. No te olvides de dejar tu opinión sobre esta problemática.',
+   	'ready_to_share' 			=> '¿Estás listo para compartir tus ideas?',
+   	'you_can_sing_up' 			=> 'Usted puede inscribirse en Yoaky para compartir sus ideas',
+   	'see_more' 					=> 'Ver más'
+
+);
